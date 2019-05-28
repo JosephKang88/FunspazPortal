2d2115212c5bfc9a48011a1efc386c7dd8f5e50c
JosephKang88 <39254141+JosephKang88@users.noreply.github.com>
2019-05-28 14:14:57 +0700

    Add project files.

---
+++ FunspazPortal/Controllers/ListController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FunspazPortal.Controllers
+{
+    public class ListController : Controller
+    {
+        // GET: Beauty
+        public ActionResult Beauty()
+        {
+            return View();
+        }
+
+        // GET: Love
+        public ActionResult Love()
+        {
+            return View();
+        }
+
+        // GET: Entreprenuer
+        public ActionResult Entreprenuer()
+        {
+            return View();
+        }
+
+        // GET: Travel
+        public ActionResult Travel()
+        {
+            return View();
+        }
+
+        // GET: Weather
+        public ActionResult Weather()
+        {
+            return View();
+        }
+
+        // GET: Funny
+        public ActionResult Funny()
+        {
+            return View();
+        }
+    }
+}
